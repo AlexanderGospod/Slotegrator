c10d94c8cdd0dc739c389eaec55b7cbc0e257b26
AlexanderGospod <106409043+AlexanderGospod@users.noreply.github.com>
2022-05-28 11:54:54 +0300

    Update ApiTestTask.java
    Добавил комментарии

--- Slotegrator/src/test/java/api/ApiTestTask.java
+++ Slotegrator/src/test/java/api/ApiTestTask.java
@@ -16,7 +16,7 @@ public class ApiTestTask {
     @Test
     public void getGuestSToken()  {
         MethodsForAuthorization methodsForAuthorization = new MethodsForAuthorization();
-        SuccessfulGetAGuestToken successGetToken = methodsForAuthorization.gettingAGuestToken();
+        SuccessfulGetAGuestToken successGetToken = methodsForAuthorization.gettingAGuestToken(); 
         Assert.assertTrue(successGetToken.getAccess_token().trim().length() != 0);
     }
     @Test
@@ -26,7 +26,9 @@ public class ApiTestTask {
         SuccessfulGetAGuestToken successGetToken = methodsForAuthorization.gettingAGuestToken();
         //aсt
         MethodsForGamerRegistration methodsForGamerRegistration = new MethodsForGamerRegistration();
-        SuccessfulRegistrationGamer successfulRegistrationGamer = methodsForGamerRegistration.registrationANewGamer(successGetToken.getAccess_token());
+        // Метод регистрации нового игрока принимает токен гостя, а возращает объект с телом ответа сервера 
+        SuccessfulRegistrationGamer successfulRegistrationGamer = methodsForGamerRegistration.registrationANewGamer(successGetToken.getAccess_token()); 
+        // Метод сравнения принимает объект с телом ответа сервера после регистарции игрока, и сравнивает с ожидаемыми параметрами ответа   
         methodsForGamerRegistration.checkingTheReceivedResponse(successfulRegistrationGamer);
     }
     @Test
@@ -37,6 +39,7 @@ public class ApiTestTask {
         MethodsForGamerRegistration methodsForGamerRegistration = new MethodsForGamerRegistration();
         SuccessfulRegistrationGamer successfulRegistrationGamer = methodsForGamerRegistration.registrationANewGamer(successGetToken.getAccess_token());
         //act
+        // Метод авторизации принимает логин и пароль, а возращает обект с телом ответа сервера
         SuccessfulGetATokenAfterAuthorization successfulGetATokenAfterAuthorization = methodsForAuthorization.gettingAGuestTokenAfterAuthorization(
                 methodsForGamerRegistration.getUserName(), methodsForGamerRegistration.getUserPassword());
         Assert.assertTrue(successfulGetATokenAfterAuthorization.getAccess_token().trim().length() != 0);
@@ -52,8 +55,11 @@ public class ApiTestTask {
                 methodsForGamerRegistration.getUserName(), methodsForGamerRegistration.getUserPassword());
         //act
         MethodsForGetUserData methodsForGetUserData = new MethodsForGetUserData();
+        // Метод запроса данных текущего игрока принимает токен и id, а возращает объект с телом ответа сервера 
         SuccessfulGetUserData successfulGetUserData = methodsForGetUserData.gettingYourUserData(
                 successfulGetATokenAfterAuthorization.getAccess_token(), successfulRegistrationGamer.getId());
+        // Метод сравнения принимает объект с телом ответа сервера после запроса данных текущего игрока (+ логин, емаил, имя, отчество)
+        //,  и сравнивает с ожидаемыми параметрами ответа   
         methodsForGetUserData.checkingTheReceivedUserData(successfulGetUserData, successfulRegistrationGamer.getUsername()
                 , successfulRegistrationGamer.getEmail(), successfulRegistrationGamer.getName(), successfulRegistrationGamer.getSurname());
     }
@@ -68,6 +74,7 @@ public class ApiTestTask {
                 methodsForGamerRegistration.getUserName(), methodsForGamerRegistration.getUserPassword());
         //act
         MethodsForGetUserData methodsForGetUserData = new MethodsForGetUserData();
+        // Метод запроса данных игрока принимает токен, а возращает объект с телом ответа сервера (ожидается 404 ошибка)
         SuccessfulGetUserData successfulGetUserData = methodsForGetUserData.gettingYourOtherPlayerSData(
                 successfulGetATokenAfterAuthorization.getAccess_token());
     }
